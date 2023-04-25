a818210186e32c7144f97a8b2a6e457f430a36df
ElArquitectorgo <mega2814s@gmail.com>
2023-04-25 12:57:06 +0200

    Removed convert methods

--- src/main/java/bmicalc/UsaDecorator.java
+++ src/main/java/bmicalc/UsaDecorator.java
@@ -10,7 +10,7 @@ public class UsaDecorator extends IMCBaseDecorator {
 
     @Override
     public Map<Double, String> imc(double altura, double peso) {
-        Map<Double, String> res = proxy.imc(convertFootToMeters(altura), convertPoundToKg(peso));
+        Map<Double, String> res = proxy.imc(Conversor.convertFootToMeters(altura), Conversor.convertPoundToKg(peso));
         double imc = res.keySet().iterator().next();
         System.out.println(String.format("The person with height %.2f meters and %.2f kilos of weight has an IMC of %.2f.", altura, peso, imc));
         return res;
@@ -19,15 +19,7 @@ public class UsaDecorator extends IMCBaseDecorator {
     @Override
     public boolean tieneObesidadAbdominal(char genero, double circunferencia) {
         // La conversión es a metros pero nuestro método obesidad usa cm, por eso dividimos.
-        circunferencia = convertFootToMeters(circunferencia) / 100;
+        circunferencia = Conversor.convertFootToMeters(circunferencia) / 100;
         return proxy.tieneObesidadAbdominal(genero, circunferencia);
     }
-
-    private double convertFootToMeters(double altura) {
-        return altura /= 3.28;
-    }
-    private double convertPoundToKg(double peso) {
-        return peso /= 2.2;
-    }
-    
 }
