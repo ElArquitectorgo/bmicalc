1f11dac1ab9eab6fbf4c4f2703c6fe6a1ff671e8
ElArquitectorgo <mega2814s@gmail.com>
2023-04-25 12:57:22 +0200

    Added general conversor

---
+++ src/main/java/bmicalc/Conversor.java
@@ -0,0 +1,10 @@
+package bmicalc;
+
+public class Conversor {
+    public static double convertFootToMeters(double altura) {
+        return altura /= 3.28;
+    }
+    public static double convertPoundToKg(double peso) {
+        return peso /= 2.2;
+    }
+}
